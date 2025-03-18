0b771d960860868ba49645b05086cdb89ad4c2ff
dardorsh <65073890+dardorsh@users.noreply.github.com>
2025-03-18 10:57:02 +0300

    add hw

--- src/main/java/qa/guru/Main.java
+++ src/main/java/qa/guru/Main.java
@@ -2,6 +2,41 @@ package qa.guru;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // применить несколько арифметических операций ( + , -, * , /) над двумя примитивами типа int
+        int a = 10;
+        int b = 3;
+
+        System.out.println(a + b);
+        System.out.println(b - a);
+        System.out.println(a * b);
+        System.out.println(a / b);
+
+        // применить несколько арифметических операций над int и double в одном выражении
+        int x = 5;
+        double y = 2.5;
+
+        double result = x + y * 2 - x / y;
+        System.out.println(result);
+
+        //применить несколько логических операций ( < , >, >=, <= )
+        int num1 = 7;
+        int num2 = 10;
+
+        boolean isLess = num1 < num2;
+        boolean isGreater = num1 > num2;
+        boolean isGreaterOrEqual = num1 >= 7;
+        boolean isLessOrEqual = num2 <= 10;
+
+        System.out.println(isLess);
+        System.out.println(isGreater);
+        System.out.println(isGreaterOrEqual);
+        System.out.println(isLessOrEqual);
+
+        //переполнение
+        int maxInt = Integer.MAX_VALUE;
+        int overflow = maxInt + 1;
+        System.out.println(overflow);
+
     }
 }
